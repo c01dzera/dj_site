093abf60967387cc59e48c7330aa8461791ba9d1
Vilen_Bakov <bakov.vilen@yandex.ru>
2022-07-21 18:00:56 +0500

    test

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$hg1%brgl$fuilcyvigiy7u&tuu5dl(0brkk7$#6x8+*&+zp=7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,3 +6,21 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse('<h1>Страница приложения women<h1/>')
+
+
+def categories(request, catid):
+    if request.POST:
+        print(request.POST)
+
+    return HttpResponse(f'<h1>Категории<h1/><p>{catid}<p/>')
+
+
+def archive(request, year):
+    if int(year) > 2022:
+        raise Http404()
+
+    return HttpResponse(f'<h1>Архив по годам<h1/><p>{year}<p/>')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена<h1/>')
